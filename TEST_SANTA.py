47f9855b1cb0bf73aeae9b16d043a1e7493dea97
ChinmayThanekar <84039458+ChinmayThanekar@users.noreply.github.com>
2025-12-19 19:08:48 +0530

    Update TEST_SANTA.py
    test

--- TEST_SANTA.py
+++ TEST_SANTA.py
@@ -29,7 +29,11 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
+st.audio(
+    "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+    format="audio/mp3",
+    start_time=0
+)
 # ---- Snowflakes ----
 for i in range(25):
     st.markdown(
@@ -91,3 +95,4 @@ if st.button("Reveal Magic ✨") and answer:
     time.sleep(1)
 
 st.caption("🎶 Snow falling, Santa listening...")
+
